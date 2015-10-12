7298e89db51971cf1a37d7ed2d2378c6e1f382a0
Tam Ho <tam.ho@alumni.ubc.ca>
2015-10-12 11:09:09 -0700

    Add server-proxy API to search/browse for recipes, and go to next result page New infoUpdate and message callback for new connections to register for server's 	database changes or other notifications from server Add brief validation functions for some models Make Categories' Region array an enum-like object like COURSE and TIME_OF_DAY

--- recipal-client/www/js/server-proxy.js
+++ recipal-client/www/js/server-proxy.js
@@ -3,69 +3,112 @@
 /*
 connect(connectCb, disconnectCb) :
 connectCb: invoked on connection success
-disconnectCb: is invoked whenever connection is interrupted
+disconnectCb: invoked whenever connection is interrupted
+infoUpdateCb: invoked when saved information (recipe, profile, categories, etc)
+	is outdated, passed in argument is the updated info array (InfoUpdate objects)
+newMessageCb: invoked when there's any other notification from server,
+	passed in argument is the message content (str)
 
-getCategories(cb) :
-cb: invoked with the 
+getRecipes(query)
+query: the Query object used to search for recipes
+
+nextResults(resultHead)
+repeat the search query stored in resultHead, but advance past lastId,
+	calling the same callback in resultHead when results are returned
 */ 
 var server = {
 	socket: null,
 	isConnected: false,
-	connect: function(connectCb, disconnectCb) {
+	connect: function(connectCb, disconnectCb, infoUpdateCb,
+		newMessageCb) {
 		if (server.isConnected) {
 			invokeFunc(connectCb);
 			return;
 		}
 		this.socket = io.connect('http://localhost:3000');
-		this.initSocket(connectCb, disconnectCb);
+		this.initSocket(connectCb, disconnectCb, infoUpdateCb,
+		newMessageCb);
 	},	
-	initSocket: function(connectCb, disconnectCb) {
+	initSocket: function(connectCb, disconnectCb, infoUpdateCb,
+		newMessageCb) {
 		this.socket.on('connect', function(categories) {
 			console.log("Connected");
 			this.isConnected = true;
 			invokeFunc(connectCb);
 		});
-
 		this.socket.on('disconnect', function() {
 			console.log("Disconnected");
 			this.isConnected = false;
 			invokeFunc(disconnectCb);
-		// 	socket.emit('my other event', { my: 'data' });
+		});
+		this.socket.on('info-update', function(infoUpdateArr) {
+			console.log("New info incoming");
+			//handleInfoUpdate(infoUpdateArr); change the 3 enum-like objects 
+			invokeFunc(infoUpdateCb, infoUpdateArr);
+		});
+		this.socket.on('message', function(message) {
+			console.log("New message incoming");
+			invokeFunc(newMessageCb, message);
+		});
+		this.socket.on('recipe-list', function(recipeArr, resultHead) {
+			console.log("Receiving recipe list");
+			invokeFunc(resultHead.callback, recipeArr, resultHead);
 		});
 	},
-	getCategories: function(callback) {
-		//this.socket
-	}, 
-	getRecipes: function(categories, maxNumResults, callback) {
-		this.socket.emit('recipes', {categories: categories, 
-			maxNumResults: maxNumResults});
-		//this.socket.on('recipes',);
+	getRecipes: function(query) {
+		console.log("send get-recipes");
+		this.socket.emit('get-recipes', query);
+	},
+	nextResults: function(resultHead) {
+		console.log("send next-results");
+		this.socket.emit('next-results', resultHead);
 	}
-
 };
 
+// TODO: a better way to combine these 3?
 function invokeFunc(func) {
 	if (typeof func === "function" && func !== null)
 		func();
 }
 
-function validateRecipe(recipe) {
-	if (typeof recipe.id !== "number" 
-		|| recipe.id <= 0 
-		|| recipe.id % 1 != 0) 
-		throw "Invalid Id";
+function invokeFunc(func, arg1) {
+	if (typeof func === "function" && func !== null)
+		func(arg1);
+}
 
-	if (typeof recipe.authorId !== "number" 
-		|| recipe.authorId <= 0 
-		|| recipe.authorId % 1 != 0) 
-		throw "Invalid Id";
+function invokeFunc(func, arg1, arg2) {
+	if (typeof func === "function" && func !== null)
+		func(arg1, arg2);
+}
+
+// Check that id is a positive integer
+function validateId(id) {
+	if (typeof id === "number" && id % 1.0 == 0.0 && id > 0)
+		return true;
+	console.log("Invalid Id: " + id);
+	return false;
+}
+
+// TODO: Do more checking here, essentially briefly checking everything in models.js,
+// Plus query and resultHead check, even though the server should be checking
+// extensively as well (stricter than client)
+function validateRecipe(recipe) {
+	return validateId(recipe.id) && validateId(recipe.authorId);
 }
 
 function validateAuthor(author) {
-	if (typeof author.id !== "number" 
-		|| author.id <= 0 
-		|| author.id % 1 != 0) 
-		throw "Invalid Id";	
+	return validateId(author.id);
 }
 
+function validateSearchCategories(categories) {
+	return true;
+}
+
+function validateQuery(query) {
+	return validateSearchCategories(query.categories);	
+}
+
+function validateResultHead(resultHead) {
+	return validateId(resultHead.lastId) && validateQuery(resultHead.query);
+}
 
--- recipal-common/models.js
+++ recipal-common/models.js
@@ -1,47 +1,58 @@
 "use strict";
+
+// Only add new ones at the end!
+var TIME_OF_DAY_ARR = {breakfast: 0, lunch: 1, dinner: 2};
+var COURSE_ARR = {appetizer: 0, snack: 1, entree: 2, dessert: 3};
+var REGION_ARR = {};
+var UPDATE_TYPE = {categories: 0, recipe: 1, account: 2};
+
 /* 
 id: unique id (int > 0)
 name: name of recipe (str)
 authorId: id of the Author object(int > 0)
-category: Category object this recipe belongs to
-ingredientArr: non-empty list of ingredient (element: str)
+categories: Categories object this recipe belongs to
+ingredientArr: non-empty list of ingredient (element: str) TODO: not putting ingredient in categories means cannot browse recipe by ingredients (have to search using keywords)
 numSteps: number of cooking steps (int > 0)
 */
 var recipe = {
 	id: 0,
 	name: "",
 	authorId: 0,
-	category: null,
+	categories: null,
 	ingredientArr: [],
 	numSteps: 0,
-	recipe: function(id, name, authorId, category, ingredientArr, numSteps) {
+	recipe: function(id, name, authorId, categories, ingredientArr, numSteps) {
 		this.id = id;
 		this.name = name;
 		this.authorId = authorId;
-		this.category = category;
+		this.categories = categories;
 		this.ingredientArr = ingredientArr;
 		this.numSteps = numSteps;
 	}
 };
 
 /*
-regionArr: relative geographic origins of the food (element: str) // TODO: Map?
+Category property of a recipe
+regionArr: relative geographic origins of the food (element: int >= 0) // TODO: Map?
 rating: rating of recipe, a percentage in decimal form (number 0.0->1.0)
 difficulty: difficulty of recipe, a percentage in decimal form (number 0.0->1.0)
 suggestedTime: suggested time in minutes to finish (int > 0) (TODO: scaled in terms of difficulty?)
 timeOfDayArr: times of day the meal is normally eaten (element: int >= 0)
 courseArr: the courses the meal is normally served as (element: int >= 0)
-elements in timeOfDayArr and courseArr correspond to indices in 
-	TIME_OF_DAY and COURSE, respectively
+
+Elements in regionArr, timeOfDayArr and courseArr corresponds to values in 
+	3 enum-like objects with same name
+Note: If browsing/searching for recipes, this object can also be used as criteria,
+	so rating, difficulty and suggestedTime can become an array of 2 (a range)
 */
-var category = {
+var categories = {
 	regionArr: [],
 	rating: 0.0,
 	difficulty: 0.0,
 	suggestedTime: 0,
 	timeOfDayArr: [],
 	courses: [],
-	category: function(regions, rating, difficulty, suggestedTime, 
+	categories: function(regions, rating, difficulty, suggestedTime, 
 		timesOfDay, courses) {
 		this.regions = regions;
 		this.rating = rating;
@@ -51,10 +62,38 @@ var category = {
 		this.courses = courses;
 	} 
 };
-//                  0             1        2
-var TIME_OF_DAY = ["breakfast" , "lunch", "dinner"]; //ADD NEW ONES AT THE END!!!
-//                  0          1          2          3
-var COURSE = ["Appetizers", "Snacks", "Entrees", "Dessert"]; //ADD NEW ONES AT THE END!!!
+
+/* 
+Search query
+categories: search Categories object (see its Note section)
+maxNumResults: max number of results to be returned by server
+callback: invoked when receiving search results and result head,
+	passing them as arguments
+*/
+var query = {
+	categories: null, 
+	maxNumResults: 0, 
+	callback: null,
+	query: function(categories, maxNumResults, callback) {
+		this.categories = categories;
+		this.maxNumResults = maxNumResults;
+		this.callback = callback;
+	}
+};
+
+/*
+"Header" of a returned search, contains the original search Query object, 
+	and id of last recipe
+TODO: use id array instead of sending the resultHead back and forth
+*/
+var resultHead = {
+	query: null,
+	lastId: 0,
+	result: function(query, lastId) {
+		this.query = query;
+		this.lastId = lastId;
+	}
+};
 
 /*
 text: step description
@@ -116,5 +155,15 @@ var author = {
 	}
 };
 
-
-
+/*
+updateType (int >= 0) corresponds to value of enum-like object
+	with same name 
+*/
+var infoUpdate = {
+	updateType: 0,
+	newInfo: null,
+	infoUpdate: function(updateType, newInfo) {
+		this.updateType = updateType;
+		this.newInfo = newInfo;
+	}
+};
