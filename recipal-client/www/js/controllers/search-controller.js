fd1c06991d4c008fe3a8a0312cc7d847b0b5f38c
Tam Ho <tam.ho@alumni.ubc.ca>
2015-11-18 23:29:34 -0800

    * Bug fix for search * Change names of some unused files adding .old extension

--- recipal-client/www/js/controllers/search-controller.js
+++ recipal-client/www/js/controllers/search-controller.js
@@ -2,14 +2,14 @@
 
 angular.module('starter.controllers', [])
 .controller('SearchCtrl', function($scope, $ionicModal, $timeout) {
-    $scope.searchInput = "breakfast";
+    $scope.searchInput = {};
 
     $scope.search = function() {
-        console.log($scope.searchInput);
+        console.log($scope.searchInput.text);
         
         // socket.emit('search',new Query(query.name, query.author, query.rating, 
         //     query.difficulty, query.ingredients, query.categories));
-        socket.emit('search', new Query("", "", {min: 0, max: 5}, {min: 0, max: 5}, null, [$scope.searchInput]));
+        socket.emit('search', new Query("", "", {min: 0, max: 5}, {min: 0, max: 5}, null, [$scope.searchInput.text]));
         socket.on('search', function(recipeArr) {
             console.log("DEBUG: Receive search results");
             searchModel.setResults(recipeArr);
