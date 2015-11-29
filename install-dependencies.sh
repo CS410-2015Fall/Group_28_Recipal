ede3ee3d70f528e64a700236db1721ac84e9d314
Tam Ho <tam.ho@alumni.ubc.ca>
2015-11-28 17:21:58 -0800

    * Baby one more time!

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,9 @@
-cd recipal-server && npm install
+cd recipal-server
+npm install
 cd ..
 cd tests
 npm install
-cd node_modules/hackify-server && npm install
+cd node_modules
+cd hackify-server
+npm install
 bower install
