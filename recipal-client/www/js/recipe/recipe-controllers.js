e14a7ec9aa38938bc3afbf0f5782dd5351c4aa3d
Michelle Nguyen <mn0394@gmail.com>
2015-11-29 16:21:29 -0800

    Ingredients list and next button working; still need to work on next, back and timer

--- recipal-client/www/js/recipe/recipe-controllers.js
+++ recipal-client/www/js/recipe/recipe-controllers.js
@@ -3,14 +3,59 @@
 angular.module('recipe.controllers', [])
 .controller('RecipeCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
 	var recipeCtrl = this;
+	$scope.recipe = {};
+	var ingredientsCollected = 0;
+	var ingredientsNeeded = 0;
+	var stepCounter = 1;
+	
 	var onRecipeSelect = function(recipe) {
-		console.log(JSON.stringify(recipe));
-		// TODO: Set current recipe to this; 
+		console.log(JSON.stringify(recipe)); 
+		$scope.recipe = recipe;
+		ingredientsNeeded = $scope.recipe.ingredients.length;
+		$scope.$evalAsync();		
 	}
-
+	
 	$rootScope.$on('setRecipe', function(event, recipe) {
 		console.log("received event");
 		onRecipeSelect(recipe);
+		document.getElementById('instructions-container').style.display = 'none';
 	});
-
+	
+	recipeCtrl.ingredientsChecker = function() {
+		ingredientsCollected++;
+		
+		if (ingredientsCollected === ingredientsNeeded) {
+				console.log("got all");
+				document.getElementById('ingredientslist-container').style.display = 'none';
+				document.getElementById('instructions-container').style.display = 'block';
+		}		
+	}
+	
+	recipeCtrl.displayClock = function() {
+		var clockDisplay = document.getElementById('clockdiv');
+		
+	}
+	
+	recipeCtrl.next = function() {
+		console.log("next step");
+		if (stepCounter < $scope.recipe.steps.length) {
+			document.getElementById("instructions").innerHTML = $scope.recipe.steps[stepCounter].description;
+			stepCounter++;
+		} else {
+		}
+	}
+	
+	recipeCtrl.back = function() {
+		// CHECK IF ON FIRST STEP; IF TRUE, DISPLAY INGREDIENTS
+		// document.getElementById('ingredientslist-container').style.display = "block";
+		// document.getElementById('instructions-container').style.display = "none";
+		console.log("back");
+		if (stepCounter > 0) {
+			document.getElementById("instructions").innerHTML = $scope.recipe.steps[stepCounter].description;
+			stepCounter--;
+		} else {
+		}
+	}
+	
+	
 }]);
