90c8970097b65fd83e68fd02afa7fbc614f0cc6a
Michelle Nguyen <mn0394@gmail.com>
2015-12-03 19:49:44 -0800

    changes to search page

--- recipal-client/www/js/account/login-controllers.js
+++ recipal-client/www/js/account/login-controllers.js
@@ -4,57 +4,56 @@ angular.module('account.loginControllers', ['account.services', 'settings.servic
 .controller('LoginCtrl', function($scope, $state, $ionicHistory, accountService, settingsService){
 	this.loginData = {username: accountService.accountInfo.username,
 		password: accountService.accountInfo.password};
-		$scope.status = accountService.status;
+		$scope.loginStatus = "";
+		$scope.signupStatus = "";
+		$scope.forgotpassStatus = "";
 		$scope.settings = settingsService.settings;
 
 		this.login = function() {
 			console.log(JSON.stringify($scope.settings));
 			accountService.login(this.loginData, 
 				function(data) {
-					$scope.status = accountService.status;
+			//		$scope.status = accountService.status;
 					$scope.$evalAsync();
 				// Go to account page and turn off back button to prevent going back to login page
 				$ionicHistory.nextViewOptions({ disableBack: true });
 				$state.go('app.account');
 
 			}, function() {
-				$scope.status = accountService.status;
+				$scope.loginStatus = accountService.status;
 				$scope.$evalAsync();
 			});
 		};
 	})
 .controller('CreateAccountCtrl', function($scope, $state, accountService) {
 	this.createData = {};
-	$scope.status = accountService.status;
 
 	this.createAccount = function() {
 		accountService.createAccount(this.createData, 
 			function(data) {
-				$scope.status = accountService.status;
 				$scope.$evalAsync();
 				// Go back to log in page
 				$state.go('app.login');
 				
 			}, function errorCallback() {
-				$scope.status = accountService.status;
+				$scope.signupStatus = accountService.status;
 				$scope.$evalAsync();
 			});
 	};
 })
 .controller('ForgotPassCtrl', function($scope, $state, accountService) {
 	this.resetData = {};
-	$scope.status = accountService.status;
 
 	this.resetPass = function() {
 		accountService.resetPassword(this.resetData, 
 			function(data) {
-				$scope.status = accountService.status;
+				$scope.forgotpassStatus = accountService.status;
 				$scope.$evalAsync();
 				// Go back to log in page
 				$state.go('app.login');
 				
 			}, function errorCallback() {
-				$scope.status = accountService.status;
+				$scope.forgotpassStatus = accountService.status;
 				$scope.$evalAsync();
 			});
 	};
